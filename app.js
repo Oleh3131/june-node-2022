0efc22e28729e9a33f5637f2413eca55bd0986d1
Oleh3131 <oshevskyioleh@gmail.com>
2022-11-10 19:10:15 +0300

    FS

--- app.js
+++ app.js
@@ -1,2 +1,145 @@
+const fs = require('node:fs');
+
+
+// 1)ПЕРШИЙ СПОСІБ----------------------------------------------------------------------------------------
+
+fs.readdir('./men', {withFileTypes: true}, (err, files) => {
+
+    for (let file of files) {
+
+        if (file.isFile()) {
+
+            fs.readFile(`./men/${file.name}`, (err, data) => {
+
+                if (err) {
+
+                    console.log(err);
+
+                } else {
+
+                    const object = JSON.parse(data.toString());
+
+                    if (object.gender === 'female') {
+
+                        fs.rename(`./men/${file.name}`, `./women/${file.name}`, (err) => {
+
+                            console.log(err);
+
+                        })
+                    }
+                }
+            });
+        }
+    }
+});
+
+
+fs.readdir('./women', {withFileTypes: true}, (err, files) => {
+
+    for (let file of files) {
+
+        if (file.isFile()) {
+
+            fs.readFile(`./women/${file.name}`, (err, data) => {
+
+                if (err) {
+
+                    console.log(err);
+
+                } else {
+
+                    const object = JSON.parse(data.toString());
+
+                    if (object.gender === 'male') {
+
+                        fs.rename(`./women/${file.name}`, `./men/${file.name}`, (err) => {
+
+                            console.log(err);
+
+                        })
+                    }
+                }
+            });
+        }
+    }
+});
+
+
+
+// 2)ДРУГИЙ СПОСІБ-------------------------------------------------------------------------------------
+
+
+// fs.readdir('./men', (err, files) => {
+//
+//     console.log(files);
+//
+//     for (let file of files) {
+//
+//         fs.stat(`./men/${file}`, (err, stats) => {
+//
+//             if (stats.isFile()) {
+//
+//                 fs.readFile(`./men/${file}`, (err, data) => {
+//
+//
+//                     if (err) {
+//
+//                         console.log(err);
+//
+//                     } else {
+//
+//                         const object = JSON.parse(data.toString());
+//
+//                         if (object.gender === 'female') {
+//
+//                             fs.rename(`./men/${file}`, `./women/${file}`, (err) => {
+//
+//                                 console.log(err);
+//
+//                             })
+//                         }
+//                     }
+//                 });
+//             }
+//         })
+//     }
+// });
+//
+//
+// fs.readdir('./women', (err, files) => {
+//
+//     console.log(files);
+//
+//     for (let file of files) {
+//
+//         fs.stat(`./women/${file}`, (err, stats) => {
+//
+//             if (stats.isFile()) {
+//
+//                 fs.readFile(`./women/${file}`, (err, data) => {
+//
+//
+//                     if (err) {
+//
+//                         console.log(err);
+//
+//                     } else {
+//
+//                         const object = JSON.parse(data.toString());
+//
+//                         if (object.gender === 'male') {
+//
+//                             fs.rename(`./women/${file}`, `./men/${file}`, (err) => {
+//
+//                                 console.log(err);
+//
+//                             })
+//                         }
+//                     }
+//                 });
+//             }
+//         })
+//     }
+// });
 
 
