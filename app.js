e2831aefd6ae167073791ca172fa24f032b32469
Oleh3131 <oshevskyioleh@gmail.com>
2022-11-15 23:40:50 +0300

    express

--- app.js
+++ app.js
@@ -1,2 +1,84 @@
+const express = require('express');
 
+const app = express();
+
+const userDb = require('./dataBase/users');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/users', (req, res) => {
+
+    console.log('USERS EMDPOINT');
+
+    //Не мона послати два response
+    // res.json({user: 'Olejo'});
+
+    //Приймає лише стрінгу
+    // res.end('It is ok');
+
+    // Приймає статус
+    // res.status(402).json('It is ok');
+
+    // Посилання файлику(вказуємо шлях до нашого файлу)
+    // res.sendFile('./');
+
+
+    res.json(userDb);
+
+});
+
+
+app.get('/users/:userId',(req, res)=>{
+
+    console.log(req.params);
+
+    const {userId} = req.params;
+
+    res.json(userDb[userId]);
+
+});
+
+
+app.post('/users',(req, res)=>{
+
+    const userInfo = req.body;
+
+    userDb.push(userInfo);
+
+    console.log(userInfo);
+
+    res.status(201).json('Created');
+
+});
+
+
+
+app.put("/users/:userId",(req,res)=>{
+
+    const newUserInfo = req.body;
+
+    const userId = req.params.userId;
+
+    userDb[userId] = newUserInfo;
+
+    res.json('Updated');
+
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(5000,()=>{
+
+    console.log('Server listen 5000');
+
+})
 
---
+++ dataBase/users.js
@@ -0,0 +1,8 @@
+module.exports=[
+    {name:'Oleh',age:25,status:true},
+    {name:'Roma',age:52,status:false},
+    {name:'Max',age:15,status:false},
+    {name:'Misha',age:31,status:true},
+    {name:'Mari',age:45,status:true},
+    {name:'Sasha',age:18,status:false}
+]
